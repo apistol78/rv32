e0bcf89eea246e51cc348aa38e4f41a40b790de3
Anders Pistol <anders.pistol@doctorentertainment.com>
2021-12-20 10:39:32 +0100

    RV32: Fixed FIFO.

--- code/Rv32H/FIFO.v
+++ code/Rv32H/FIFO.v
@@ -1,20 +1,21 @@
 module FIFO(
-	output wire o_empty,
-	output wire o_full,
+	output reg o_empty,
+	output reg o_full,
 
 	input wire i_write,
 	input wire [7:0] i_wdata,
 
 	input wire i_read,
-	output wire [7:0] o_rdata
+	output reg [7:0] o_rdata
 );
 	reg [7:0] data [0:63];
 	reg [7:0] in = 0;
 	reg [7:0] out = 0;
 
-	assign o_empty = (in == out);
-	assign o_full = (((out + 1) & 63) == in);
-	assign o_rdata = data[out];
+	always @ (*) begin
+		o_empty <= (in == out);
+		o_full <= (((in + 1) & 63) == out);
+	end
 
 	always @ (posedge i_write) begin
 		if (!o_full) begin
@@ -25,6 +26,7 @@ module FIFO(
 
 	always @ (posedge i_read) begin
 		if (!o_empty) begin
+			o_rdata <= data[out];
 			out <= (out + 1) & 63;
 		end
 	end
