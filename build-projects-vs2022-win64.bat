9d09195afc83b291c1623b1d2794bc0e7f6a4c5e
Anders Pistol <anders.pistol@doctorentertainment.com>
2022-01-03 17:18:16 +0100

    RV32: Generate emulator files when building VS solution.

--- build-projects-vs2022-win64.bat
+++ build-projects-vs2022-win64.bat
@@ -2,6 +2,9 @@
 
 call %~dp0..\traktor\scripts\config.bat
 
+:: Generate instructions.
+"%TRAKTOR_HOME%\bin\win64\releasestatic\Traktor.Run.App" code/Instructions.run cpp > code/Rv32/Instructions.inl
+
 %SOLUTIONBUILDER% ^
 	-f=msvc ^
 	-p=$(TRAKTOR_HOME)\resources\build\configurations\msvc-2022-win64.xml ^
