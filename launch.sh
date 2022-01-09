8b92713d25387d914e454db5182c08c5a715b4c5
Anders Pistol <anders.pistol@doctorentertainment.com>
2022-01-10 00:07:05 +0100

    RV32: Launcher script.

--- launch.sh
+++ launch.sh
@@ -2,7 +2,7 @@
 
 . ./config.sh
 
-./build/linux/ReleaseStatic/Launch -upload=Bare.hex -stack=0x26FFFFF0 -t -port=0
+./build/linux/ReleaseStatic/Launch -upload=Bare.hex -stack=0x27FFFFF0 -t -port=0
 #./build/linux/ReleaseStatic/Launch -upload=Bare.hex -stack=0x103FFFF0 -t -port=0
 
 #./build/linux/ReleaseStatic/Rv32 -hex=Bare.hex -stack=0x27FFFFF0
