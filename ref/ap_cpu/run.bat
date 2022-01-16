be1e5180962babd0a7dee3b19acd671b548dc2fb
Anders Pistol <anders.pistol@doctorentertainment.com>
2022-01-16 11:49:48 +0100

    RV32: Removed old CPU reference.

--- ref/ap_cpu/run.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-iverilog -o ap_cpu_tb.out ap_cpu_tb.v
-if %ERRORLEVEL% equ 0 (
-	vvp ap_cpu_tb.out
-)
--- ref/ap_cpu/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-if iverilog -o ap_cpu_tb.out ap_cpu_tb.v; then
-	vvp ap_cpu_tb.out
-else
-	echo "-- Compile error --"
-fi
