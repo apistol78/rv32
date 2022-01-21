31271d9f69628e78f8f14d5222b526f8724efe28
Anders Pistol <anders.pistol@doctorentertainment.com>
2022-01-21 18:15:40 +0100

    RV32: Fixed issue when booting from non-zero vector in icache. Also increased icache size.

--- code/Rv32H/CPU_ICache.v
+++ code/Rv32H/CPU_ICache.v
@@ -14,11 +14,11 @@ module CPU_ICache(
 	// Bus
 	output reg o_bus_request,
 	input wire i_bus_ready,
-	output reg [31:0] o_bus_address,
+	output wire [31:0] o_bus_address,
 	input wire [31:0] i_bus_rdata
 );
 
-	localparam SIZE	= 10;
+	localparam SIZE	= 12;
 	localparam RANGE = 1 << SIZE;
 
 	reg [3:0] next;
@@ -50,7 +50,6 @@ module CPU_ICache(
 
 	initial begin
 		o_bus_request = 0;
-		o_bus_address = 32'h0;
 
 		next = 0;
 		state = 0;
@@ -58,6 +57,8 @@ module CPU_ICache(
 		cache_rw = 0;
 		cache_wdata = 0;
 	end
+
+	assign o_bus_address = i_input_pc;
 	
 	always @(posedge i_clock) begin
 		state <= next;
@@ -69,9 +70,7 @@ module CPU_ICache(
 	
 		o_ready = 0;
 		o_rdata = 32'h0;
-		
 		o_bus_request = 0;
-		o_bus_address = 32'h0;
 		
 		cache_rw = 0;
 		cache_wdata = 0;
@@ -84,7 +83,6 @@ module CPU_ICache(
 					end
 					else begin
 						o_bus_request = 1;
-						o_bus_address = i_input_pc;
 						next = 2;
 					end
 				end
@@ -98,14 +96,12 @@ module CPU_ICache(
 				end
 				else begin
 					o_bus_request = 1;
-					o_bus_address = i_input_pc;
 					next = 2;
 				end
 			end
 			
 			2: begin
 				o_bus_request = 1;
-				o_bus_address = i_input_pc;
 				if (i_bus_ready) begin
 					cache_rw = 1;
 					cache_wdata = { i_bus_rdata, { i_input_pc[31:2], 2'b01 } };
