b8783e3004f1c349ebaa9de55b3aecc5c40587ff
Anders Pistol <anders.pistol@doctorentertainment.com>
2022-02-07 14:46:44 +0100

    Rv32: Optimized HDMI device in Rv32T, setting pixel in data directly.

--- code/Rv32T/HDMI.cpp
+++ code/Rv32T/HDMI.cpp
@@ -10,7 +10,7 @@ using namespace traktor;
 HDMI::HDMI()
 {
 	m_image = new drawing::Image(
-		drawing::PixelFormat::getR8G8B8X8(),
+		drawing::PixelFormat::getX8R8G8B8(),
 		640,
 		480
 	);
@@ -19,57 +19,52 @@ HDMI::HDMI()
 
 void HDMI::eval(VSoC* soc)
 {
-    auto& clk = soc->HDMI_TX_CLK;
-    auto& de = soc->HDMI_TX_DE;
-    auto& hs = soc->HDMI_TX_HS;
-    auto& txi = soc->HDMI_TX_INT;
-    auto& vs = soc->HDMI_TX_VS;
-    auto& d = soc->HDMI_TX_D;
+	auto& clk = soc->HDMI_TX_CLK;
+	auto& de = soc->HDMI_TX_DE;
+	auto& hs = soc->HDMI_TX_HS;
+	auto& txi = soc->HDMI_TX_INT;
+	auto& vs = soc->HDMI_TX_VS;
+	auto& d = soc->HDMI_TX_D;
 
-    if (vs == 0 && m_vsync == false)
-    {
-        m_vsync = true;
-        m_vpos = 0;
-    }
-    else if (vs != 0 && m_vsync == true)
-    {
-        m_vsync = false;
-    }
+	if (vs == 0 && m_vsync == false)
+	{
+		m_vsync = true;
+		m_vpos = 0;
+	}
+	else if (vs != 0 && m_vsync == true)
+	{
+		m_vsync = false;
+	}
 
-    if (hs == 0 && m_hsync == false)
-    {
-        m_hsync = true;
-        m_hpos = 0;
-        m_vpos++;
-    }
-    else if (hs != 0 && m_hsync == true)
-    {
-        m_hsync = false;
-    }
+	if (hs == 0 && m_hsync == false)
+	{
+		m_hsync = true;
+		m_hpos = 0;
+		m_vpos++;
+	}
+	else if (hs != 0 && m_hsync == true)
+	{
+		m_hsync = false;
+	}
 
-    if (!m_clk && clk)
-    {
-        int32_t x = m_hpos - 144; // 199;
-        int32_t y = m_vpos; // - 34;
-        if (x >= 0 && x < 640 && y >= 0 && y < 480)
-        {
-            uint8_t r = (d >> 16) & 255;
-            uint8_t g = (d >> 8) & 255;
-            uint8_t b = d & 255;
-
-            m_image->setPixel(x, y, Color4f::fromColor4ub(
-                Color4ub(r, g, b)
-            ));
-            m_dirty = true;
-        }
-        m_hpos++;
-    }
-    m_clk = (bool)clk;
+	if (!m_clk && clk)
+	{
+		const int32_t x = m_hpos - 144;
+		const int32_t y = m_vpos;
+		if (x >= 0 && x < 640 && y >= 0 && y < 480)
+		{
+			uint32_t* id = (uint32_t*)m_image->getData();
+			id[x + y * 640] = d;
+			m_dirty = true;
+		}
+		m_hpos++;
+	}
+	m_clk = (bool)clk;
 }
 
 bool HDMI::shouldDraw()
 {
-    bool dirty = m_dirty;
-    m_dirty = false;
-    return dirty;
+	const bool dirty = m_dirty;
+	m_dirty = false;
+	return dirty;
 }
