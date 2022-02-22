ec5f7a73daaaf5b3af95184b807f9a023be9987f
Anders Pistol <anders.pistol@doctorentertainment.com>
2022-02-22 16:45:45 +0100

    Rv32: Some minor updates to GtkWave save file etc.

--- launch.sh
+++ launch.sh
@@ -2,7 +2,7 @@
 
 . ./config.sh
 
-#./build/linux/ReleaseStatic/Launch -elf=build/rv32/ReleaseStatic/Doom -stack=0x27FFFFF0 -t -port=0
-./build/linux/ReleaseStatic/Launch -elf=build/rv32/ReleaseStatic/Doom -stack=0x1000fff0 -t -port=0
+./build/linux/ReleaseStatic/Launch -elf=build/rv32/ReleaseStatic/Quake -stack=0x20FFFFF0 -t -port=0
+#./build/linux/ReleaseStatic/Launch -elf=build/rv32/ReleaseStatic/Doom -stack=0x1000fff0 -t -port=0
 
-#./build/linux/ReleaseStatic/Rv32 -elf=build/rv32/ReleaseStatic/Bare -stack=0x27FFFFF0
+#./build/linux/ReleaseStatic/Rv32 -elf=build/rv32/ReleaseStatic/Bare -stack=0x20FFFFF0
