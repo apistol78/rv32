b14ec9e63a2ce043fa0aea667aa55738d69445d2
Anders Pistol <anders.pistol@doctorentertainment.com>
2022-02-23 17:12:30 +0100

    Rv32: Cleaned up DMA peripherial a bit.

--- rtl/peripherials/DMA.sv
+++ rtl/peripherials/DMA.sv
@@ -2,40 +2,44 @@
 `timescale 1ns/1ns
 
 module DMA(
-	input wire i_reset,
-	input wire i_clock,
+	input i_reset,
+	input i_clock,
 
-	input wire i_request,
-	input wire i_rw,
-	input wire [1:0] i_address,
-	input wire [31:0] i_wdata,
-	output reg [31:0] o_rdata,
-	output reg o_ready,
+	input i_request,
+	input i_rw,
+	input [1:0] i_address,
+	input [31:0] i_wdata,
+	output logic [31:0] o_rdata,
+	output logic o_ready,
 
 	// System
-	input wire i_stall,
+	input i_stall,
 
 	// Bus
-	output reg o_bus_rw,				// Data read/write
-	output reg o_bus_request,			// IO request.
-	input wire i_bus_ready,				// IO request ready.
-	output reg [31:0] o_bus_address,	// Address
-	input wire [31:0] i_bus_rdata,		// Read data
-	output reg [31:0] o_bus_wdata		// Write data,
+	output logic o_bus_rw,				// Data read/write
+	output logic o_bus_request,			// IO request.
+	input i_bus_ready,					// IO request ready.
+	output logic [31:0] o_bus_address,	// Address
+	input [31:0] i_bus_rdata,			// Read data
+	output logic [31:0] o_bus_wdata		// Write data,
 
 );
+	typedef enum bit [1:0]
+	{
+		READ_REQ,
+		WAIT_READ,
+		WRITE_REQ,
+		WAIT_WRITE
+	} state_t;
 
-	reg [31:0] from;
-	reg [31:0] to;
-	reg [31:0] count;
-	reg [31:0] data;
-	reg run;
-	reg [2:0] state;
+	logic [31:0] from;
+	logic [31:0] to;
+	logic [31:0] count;
+	logic [31:0] data;
+	logic run = 0;
+	state_t state = READ_REQ;
 
 	initial begin
-		run = 0;
-		state = 0;
-
 		o_ready = 0;
 		o_bus_rw = 0;
 		o_bus_request = 0;
@@ -43,12 +47,11 @@ module DMA(
 		o_bus_wdata = 0;
 	end
 
-	always @(posedge i_clock) begin
+	always_ff @(posedge i_clock) begin
 
 		o_ready <= 0;
 
 		if (i_request) begin
-
 			if (!i_rw) begin
 				if (i_address == 2'b11) begin
 					o_rdata <= run ? 32'hffff_ffff : 32'h0000_0000;
@@ -74,52 +77,50 @@ module DMA(
 					o_ready <= 1;
 				end
 			end
-
 		end
 
 		// Execute transfer.
 		if (run) begin
 			case (state)
-				0: begin
+				READ_REQ: begin
 					if (!i_stall) begin
 						o_bus_request <= 1;
 						o_bus_rw <= 0;
 						o_bus_address <= from;
-						state <= 1;
+						state <= WAIT_READ;
 					end
 				end
 
-				1: begin
+				WAIT_READ: begin
 					if (i_bus_ready) begin
 						o_bus_request <= 0;
 						data <= i_bus_rdata;
 						from <= from + 4;
-						state <= 2;
+						state <= WRITE_REQ;
 					end
 				end
 
-				2: begin
+				WRITE_REQ: begin
 					if (!i_stall) begin
 						o_bus_request <= 1;
 						o_bus_rw <= 1;
 						o_bus_address <= to;
 						o_bus_wdata <= data;
-						state <= 3;
+						state <= WAIT_WRITE;
 					end
 				end
 
-				3: begin
+				WAIT_WRITE: begin
 					if (i_bus_ready) begin
 						o_bus_request <= 0;
 						to <= to + 4;
 						if (count > 0) begin
 							count <= count - 1;
-							state <= 0;
 						end
 						else begin
 							run <= 0;
-							state <= 0;
 						end
+						state <= READ_REQ;
 					end
 				end		
 			endcase
