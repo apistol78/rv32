7d3ef7eb6a732ba7285198142f46daf51cf11de9
Anders Pistol <anders.pistol@doctorentertainment.com>
2022-03-25 13:44:37 +0100

    Rv32: Improved DMA, able to queue multiple DMA requests. Also able to clear memory using DMA (need to invalidate D$).

--- code/Kernal/HAL/DMA.c
+++ code/Kernal/HAL/DMA.c
@@ -1,13 +1,23 @@
 #include "Kernal/HAL/DMA.h"
 #include "Kernal/HAL/Interrupt.h"
 
+uint32_t dma_write(void* dst, uint32_t count, uint32_t value)
+{
+	interrupt_disable();
+	*DMA_FROM = value;
+	*DMA_TO = (uint32_t)dst;
+	*DMA_COUNT = count;
+	*DMA_RUN = 1;
+	interrupt_enable();
+}
+
 uint32_t dma_copy(void* dst, const void* src, uint32_t count)
 {
 	interrupt_disable();
 	*DMA_FROM = (uint32_t)src;
 	*DMA_TO = (uint32_t)dst;
 	*DMA_COUNT = count;
-	*DMA_RUN = 0x01010101;
+	*DMA_RUN = 2;
 	interrupt_enable();
 }
 
--- rtl/peripherials/DMA.sv
+++ rtl/peripherials/DMA.sv
@@ -22,22 +22,62 @@ module DMA(
 	output logic [31:0] o_bus_address,	// Address
 	input [31:0] i_bus_rdata,			// Read data
 	output logic [31:0] o_bus_wdata		// Write data,
-
 );
+	typedef enum bit [3:0]
+	{
+		IDLE			= 0,
+		READ_CMD		= 1,
+
+		// Write
+		W_WRITE_REQ		= 2,
+		W_WAIT_WRITE	= 3,
+
+		// Copy
+		C_READ_REQ		= 4,
+		C_WAIT_READ		= 5,
+		C_WRITE_REQ		= 6,
+		C_WAIT_WRITE	= 7
+	}
+	state_t;
+
 	typedef enum bit [1:0]
 	{
-		READ_REQ,
-		WAIT_READ,
-		WRITE_REQ,
-		WAIT_WRITE
-	} state_t;
+		WRITE	= 1,
+		COPY	= 2
+	}
+	dma_type_t;
 
-	logic [31:0] from;
-	logic [31:0] to;
-	logic [31:0] count;
+	typedef struct packed
+	{
+		dma_type_t dt;
+		logic [31:0] value_or_from;
+		logic [31:0] to;
+		logic [31:0] count;
+	}
+	dma_command_t;
+
+	wire queue_empty;
+	wire queue_full;
+	logic queue_write = 0;
+	logic queue_read = 0;
+	wire dma_command_t queue_rdata;
+	FIFO64 #(
+		.DEPTH(8),
+		.WIDTH($bits(dma_command_t))
+	) queue(
+		.i_clock(i_clock),
+		.o_empty(queue_empty),
+		.o_full(queue_full),
+		.i_write(queue_write),
+		.i_wdata(wr_command),
+		.i_read(queue_read),
+		.o_rdata(queue_rdata)
+	);
+
+	dma_command_t wr_command;
+	dma_command_t rd_command;
 	logic [31:0] data;
-	logic run = 0;
-	state_t state = READ_REQ;
+	state_t state = IDLE;
 
 	initial begin
 		o_ready = 0;
@@ -47,85 +87,137 @@ module DMA(
 		o_bus_wdata = 0;
 	end
 
+	// Receive commands and insert into queue.
 	always_ff @(posedge i_clock) begin
-
-		o_ready <= 0;
-
+		queue_write <= 0;
 		if (i_request) begin
 			if (!i_rw) begin
 				if (i_address == 2'b11) begin
-					o_rdata <= run ? 32'hffff_ffff : 32'h0000_0000;
+					o_rdata <= (!queue_empty || state != IDLE) ? 32'hffff_ffff : 32'h0000_0000;
 					o_ready <= 1;
 				end
 			end
 			else begin
 				// Receive commands from CPU.
 				if (i_address == 2'b00) begin
-					from <= i_wdata;
+					wr_command.value_or_from <= i_wdata;
 					o_ready <= 1;
 				end
 				else if (i_address == 2'b01) begin
-					to <= i_wdata;
+					wr_command.to <= i_wdata;
 					o_ready <= 1;
 				end
 				else if (i_address == 2'b10) begin
-					count <= i_wdata;
+					wr_command.count <= i_wdata;
 					o_ready <= 1;
 				end
 				else if (i_address == 2'b11) begin
-					run <= 1;
-					o_ready <= 1;
+					wr_command.dt <= i_wdata[1:0];
+					if (!queue_full) begin
+						queue_write <= !o_ready;
+						o_ready <= 1;
+					end
 				end
 			end
 		end
+		else
+			o_ready <= 0;
+	end
 
-		// Execute transfer.
-		if (run) begin
-			case (state)
-				READ_REQ: begin
-					if (!i_stall) begin
-						o_bus_request <= 1;
-						o_bus_rw <= 0;
-						o_bus_address <= from;
-						state <= WAIT_READ;
+	// Process commands.
+	always_ff @(posedge i_clock) begin
+		queue_read <= 0;
+		case (state)
+			IDLE: begin
+				if (!queue_empty) begin
+					queue_read <= 1;
+					state <= READ_CMD;
+				end
+			end
+
+			READ_CMD: begin
+				rd_command <= queue_rdata;
+				if (queue_rdata.dt == WRITE)
+					state <= W_WRITE_REQ;
+				else if (queue_rdata.dt == COPY)
+					state <= C_READ_REQ;
+				else
+					state <= IDLE;
+			end
+
+			// Write
+
+			W_WRITE_REQ: begin
+				if (!i_stall) begin
+					o_bus_request <= 1;
+					o_bus_rw <= 1;
+					o_bus_address <= rd_command.to;
+					o_bus_wdata <= rd_command.value_or_from;
+					state <= W_WAIT_WRITE;
+				end				
+			end
+
+			W_WAIT_WRITE: begin
+				if (i_bus_ready) begin
+					o_bus_request <= 0;
+					rd_command.to <= rd_command.to + 4;
+					if (rd_command.count > 0) begin
+						rd_command.count <= rd_command.count - 1;
+						state <= W_WRITE_REQ;
+					end
+					else begin
+						state <= IDLE;
 					end
 				end
+			end
 
-				WAIT_READ: begin
-					if (i_bus_ready) begin
-						o_bus_request <= 0;
-						data <= i_bus_rdata;
-						from <= from + 4;
-						state <= WRITE_REQ;
+			// Copy
+
+			C_READ_REQ: begin
+				if (!i_stall) begin
+					o_bus_request <= 1;
+					o_bus_rw <= 0;
+					o_bus_address <= rd_command.value_or_from;
+					state <= C_WAIT_READ;
+				end
+			end
+
+			C_WAIT_READ: begin
+				if (i_bus_ready) begin
+					o_bus_request <= 0;
+					data <= i_bus_rdata;
+					rd_command.value_or_from <= rd_command.value_or_from + 4;
+					state <= C_WRITE_REQ;
+				end
+			end
+
+			C_WRITE_REQ: begin
+				if (!i_stall) begin
+					o_bus_request <= 1;
+					o_bus_rw <= 1;
+					o_bus_address <= rd_command.to;
+					o_bus_wdata <= data;
+					state <= C_WAIT_WRITE;
+				end
+			end
+
+			C_WAIT_WRITE: begin
+				if (i_bus_ready) begin
+					o_bus_request <= 0;
+					rd_command.to <= rd_command.to + 4;
+					if (rd_command.count > 0) begin
+						rd_command.count <= rd_command.count - 1;
+						state <= C_READ_REQ;
+					end
+					else begin
+						state <= IDLE;
 					end
 				end
+			end
 
-				WRITE_REQ: begin
-					if (!i_stall) begin
-						o_bus_request <= 1;
-						o_bus_rw <= 1;
-						o_bus_address <= to;
-						o_bus_wdata <= data;
-						state <= WAIT_WRITE;
-					end
-				end
-
-				WAIT_WRITE: begin
-					if (i_bus_ready) begin
-						o_bus_request <= 0;
-						to <= to + 4;
-						if (count > 0) begin
-							count <= count - 1;
-						end
-						else begin
-							run <= 0;
-						end
-						state <= READ_REQ;
-					end
-				end		
-			endcase
-		end
-
+			default:
+				state <= IDLE;
+		endcase
 	end
 
 endmodule
