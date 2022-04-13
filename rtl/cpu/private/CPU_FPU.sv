b6f9de07a058b57d875db3e48155c4bd1d2dc843
Anders Pistol <anders.pistol@doctorentertainment.com>
2022-04-13 11:10:13 +0200

    Rv32: Merged FPU add & sub instances.

--- rtl/cpu/private/CPU_FPU.sv
+++ rtl/cpu/private/CPU_FPU.sv
@@ -19,30 +19,18 @@ module CPU_FPU(
 	output [31:0] o_result
 );
 
-	wire fadd_request = (i_op == `FPU_OP_ADD) && i_request; 
-	wire fadd_ready;
-	wire [31:0] fadd_result;
-	CPU_FPU_Add fadd(
+	wire faddsub_request = (i_op == `FPU_OP_ADD || i_op == `FPU_OP_SUB) && i_request; 
+	wire faddsub_ready;
+	wire [31:0] faddsub_result;
+	wire [31:0] faddsub_op2 = (i_op == `FPU_OP_ADD) ? i_op2 : (32'h80000000 ^ i_op2);
+	CPU_FPU_Add faddsub(
 		.i_reset(i_reset),
 		.i_clock(i_clock),
-		.i_request(fadd_request),
+		.i_request(faddsub_request),
 		.i_op1(i_op1),
-		.i_op2(i_op2),
-		.o_ready(fadd_ready),
-		.o_result(fadd_result)
-	);
-
-	wire fsub_request = (i_op == `FPU_OP_SUB) && i_request; 
-	wire fsub_ready;
-	wire [31:0] fsub_result;
-	CPU_FPU_Add fsub(
-		.i_reset(i_reset),
-		.i_clock(i_clock),
-		.i_request(fsub_request),
-		.i_op1(i_op1),
-		.i_op2(32'h80000000 ^ i_op2),
-		.o_ready(fsub_ready),
-		.o_result(fsub_result)
+		.i_op2(faddsub_op2),
+		.o_ready(faddsub_ready),
+		.o_result(faddsub_result)
 	);
 
 	wire fmul_request = (i_op == `FPU_OP_MUL) && i_request; 
@@ -71,34 +59,20 @@ module CPU_FPU(
 		.o_result(fdiv_result)
 	);
 
-	wire fmadd_request = (i_op == `FPU_OP_MADD || i_op == `FPU_OP_NMADD) && i_request; 
-	wire fmadd_ready;
-	wire [31:0] fmadd_result;
-	wire [31:0] fnmadd_result = 32'h80000000 ^ fmadd_result;
-	CPU_FPU_MulAdd fmadd(
+	wire fmaddsub_request = (i_op == `FPU_OP_MADD || i_op == `FPU_OP_NMADD || i_op == `FPU_OP_MSUB || i_op == `FPU_OP_NMSUB) && i_request; 
+	wire fmaddsub_ready;
+	wire [31:0] fmaddsub_result;
+	wire [31:0] fnmaddsub_result = (32'h80000000 ^ fmaddsub_result);
+	wire [31:0] fmaddsub_op3 = (i_op == `FPU_OP_MADD || i_op == `FPU_OP_NMADD) ? i_op3 : (32'h80000000 ^ i_op3);
+	CPU_FPU_MulAdd fmaddsub(
 		.i_reset(i_reset),
 		.i_clock(i_clock),
-		.i_request(fmadd_request),
+		.i_request(fmaddsub_request),
 		.i_op1(i_op1),
 		.i_op2(i_op2),
-		.i_op3(i_op3),
-		.o_ready(fmadd_ready),
-		.o_result(fmadd_result)
-	);
-
-	wire fmsub_request = (i_op == `FPU_OP_MSUB || i_op == `FPU_OP_NMSUB) && i_request; 
-	wire fmsub_ready;
-	wire [31:0] fmsub_result;
-	wire [31:0] fnmsub_result = 32'h80000000 ^ fmsub_result;
-	CPU_FPU_MulAdd fmsub(
-		.i_reset(i_reset),
-		.i_clock(i_clock),
-		.i_request(fmsub_request),
-		.i_op1(i_op1),
-		.i_op2(i_op2),
-		.i_op3(32'h80000000 ^ i_op3),
-		.o_ready(fmsub_ready),
-		.o_result(fmsub_result)
+		.i_op3(fmaddsub_op3),
+		.o_ready(fmaddsub_ready),
+		.o_result(fmaddsub_result)
 	);
 
 	wire f2i_request = (i_op == `FPU_OP_F2I) && i_request; 
@@ -164,50 +138,50 @@ module CPU_FPU(
 
 	assign o_ready =
 		i_request & (
-			i_op == `FPU_OP_ADD			? fadd_ready	:
-			i_op == `FPU_OP_SUB			? fsub_ready	:
-			i_op == `FPU_OP_MUL			? fmul_ready	:
-			i_op == `FPU_OP_DIV			? fdiv_ready	:
-			i_op == `FPU_OP_MADD		? fmadd_ready	:
-			i_op == `FPU_OP_MSUB		? fmsub_ready	:
-			i_op == `FPU_OP_NMADD		? fmadd_ready	:
-			i_op == `FPU_OP_NMSUB		? fmsub_ready	:
-			i_op == `FPU_OP_F2I			? f2i_ready		:
-			i_op == `FPU_OP_I2F			? i2f_ready		:
-			i_op == `FPU_OP_UI2F		? i2f_ready		:
-			i_op == `FPU_OP_MOV			? 1'b1			:
-			i_op == `FPU_OP_CMP_EQUAL	? cmp_ready		:
-			i_op == `FPU_OP_CMP_LESS	? cmp_ready		:
-			i_op == `FPU_OP_CMP_LEQUAL	? cmp_ready		:
-			i_op == `FPU_OP_SGNJ		? 1'b1			:
-			i_op == `FPU_OP_SGNJN		? 1'b1			:
-			i_op == `FPU_OP_SGNJX		? 1'b1			:
-			i_op == `FPU_OP_MIN			? cmp_ready		:
-			i_op == `FPU_OP_MAX			? cmp_ready		:
+			i_op == `FPU_OP_ADD			? faddsub_ready		:
+			i_op == `FPU_OP_SUB			? faddsub_ready		:
+			i_op == `FPU_OP_MUL			? fmul_ready		:
+			i_op == `FPU_OP_DIV			? fdiv_ready		:
+			i_op == `FPU_OP_MADD		? fmaddsub_ready	:
+			i_op == `FPU_OP_MSUB		? fmaddsub_ready	:
+			i_op == `FPU_OP_NMADD		? fmaddsub_ready	:
+			i_op == `FPU_OP_NMSUB		? fmaddsub_ready	:
+			i_op == `FPU_OP_F2I			? f2i_ready			:
+			i_op == `FPU_OP_I2F			? i2f_ready			:
+			i_op == `FPU_OP_UI2F		? i2f_ready			:
+			i_op == `FPU_OP_MOV			? 1'b1				:
+			i_op == `FPU_OP_CMP_EQUAL	? cmp_ready			:
+			i_op == `FPU_OP_CMP_LESS	? cmp_ready			:
+			i_op == `FPU_OP_CMP_LEQUAL	? cmp_ready			:
+			i_op == `FPU_OP_SGNJ		? 1'b1				:
+			i_op == `FPU_OP_SGNJN		? 1'b1				:
+			i_op == `FPU_OP_SGNJX		? 1'b1				:
+			i_op == `FPU_OP_MIN			? cmp_ready			:
+			i_op == `FPU_OP_MAX			? cmp_ready			:
 			0
 		);
 
 	assign o_result =
-		i_op == `FPU_OP_ADD			? fadd_result	:
-		i_op == `FPU_OP_SUB			? fsub_result	:
-		i_op == `FPU_OP_MUL			? fmul_result	:
-		i_op == `FPU_OP_DIV			? fdiv_result	:
-		i_op == `FPU_OP_MADD		? fmadd_result	:
-		i_op == `FPU_OP_MSUB		? fmsub_result	:
-		i_op == `FPU_OP_NMADD		? fnmadd_result	:
-		i_op == `FPU_OP_NMSUB		? fnmsub_result	:
-		i_op == `FPU_OP_F2I			? f2i_result	:
-		i_op == `FPU_OP_I2F			? i2f_result	:
-		i_op == `FPU_OP_UI2F		? i2f_result	:
-		i_op == `FPU_OP_MOV			? i_op1			:
-		i_op == `FPU_OP_CMP_EQUAL	? fcmp_equal	:
-		i_op == `FPU_OP_CMP_LESS	? fcmp_less		:
-		i_op == `FPU_OP_CMP_LEQUAL	? fcmp_lequal	:
-		i_op == `FPU_OP_SGNJ		? fsgnj_result	:
-		i_op == `FPU_OP_SGNJN		? fsgnjn_result	:
-		i_op == `FPU_OP_SGNJX		? fsgnjx_result	:
-		i_op == `FPU_OP_MIN			? fmin_result	:
-		i_op == `FPU_OP_MAX			? fmax_result	:
+		i_op == `FPU_OP_ADD			? faddsub_result	:
+		i_op == `FPU_OP_SUB			? faddsub_result	:
+		i_op == `FPU_OP_MUL			? fmul_result		:
+		i_op == `FPU_OP_DIV			? fdiv_result		:
+		i_op == `FPU_OP_MADD		? fmaddsub_result	:
+		i_op == `FPU_OP_MSUB		? fmaddsub_result	:
+		i_op == `FPU_OP_NMADD		? fnmaddsub_result	:
+		i_op == `FPU_OP_NMSUB		? fnmaddsub_result	:
+		i_op == `FPU_OP_F2I			? f2i_result		:
+		i_op == `FPU_OP_I2F			? i2f_result		:
+		i_op == `FPU_OP_UI2F		? i2f_result		:
+		i_op == `FPU_OP_MOV			? i_op1				:
+		i_op == `FPU_OP_CMP_EQUAL	? fcmp_equal		:
+		i_op == `FPU_OP_CMP_LESS	? fcmp_less			:
+		i_op == `FPU_OP_CMP_LEQUAL	? fcmp_lequal		:
+		i_op == `FPU_OP_SGNJ		? fsgnj_result		:
+		i_op == `FPU_OP_SGNJN		? fsgnjn_result		:
+		i_op == `FPU_OP_SGNJX		? fsgnjx_result		:
+		i_op == `FPU_OP_MIN			? fmin_result		:
+		i_op == `FPU_OP_MAX			? fmax_result		:
 		0;
 
 endmodule
