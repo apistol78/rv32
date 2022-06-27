7d0b388b7533414e21a569527a56d3bd9932a4bd
Anders Pistol <anders.pistol@doctorentertainment.com>
2022-06-27 14:13:48 +0200

    Rv32: Fixed issues with fread, was reading too big chunks.

--- code/Runtime/CRT.c
+++ code/Runtime/CRT.c
@@ -25,8 +25,11 @@ int _open(const char* name, int flags, int mode)
 
 int _close(int file)
 {
-	int32_t fd = file - 100;
-	file_close(fd);
+	if (file > 100)
+	{
+		int32_t fd = file - 100;
+		file_close(fd);
+	}
 	return 0;
 }
 
@@ -44,18 +47,28 @@ int _fstat(int file, struct stat* st)
 		int32_t fd = file - 100;
 		st->st_mode = S_IFBLK;
 		st->st_size = file_size(fd);
+		st->st_blksize = 256;
+		st->st_blocks = file_size(fd) / 256;
 	}
 	return 0;
 }
 
 int _isatty(int file)
 {
-	return file < 100 ? 1 : 0;
+    switch (file)
+	{
+    case STDOUT_FILENO:
+    case STDERR_FILENO:
+    case STDIN_FILENO:
+        return 1;
+    default:
+        return 0;
+    }
 }
 
 int _lseek(int file, int ptr, int dir)
 {
-	if (file >= 100)
+	if (file > 100)
 	{
 		int32_t fd = file - 100;
 		return file_seek(fd, ptr, dir);
@@ -69,19 +82,19 @@ void _exit(int status)
 	for(;;);
 }
 
-void _kill(int pid, int sig)
-{
-	return;
-}
-
-int _getpid(void)
+int _kill(int pid, int sig)
 {
 	return -1;
 }
 
+int _getpid(void)
+{
+	return 1;
+}
+
 int _write(int file, char* ptr, int len)
 {
-	if (file >= 100)
+	if (file > 100)
 	{
 		int32_t fd = file - 100;
 		return file_write(fd, ptr, len);
@@ -96,10 +109,24 @@ int _write(int file, char* ptr, int len)
 
 int _read(int file, char* ptr, int len)
 {
-	if (file >= 100)
+	if (file > 100)
 	{
 		int32_t fd = file - 100;
-		return file_read(fd, ptr, len);
+
+		int nbytes = 0;
+		for (int o = 0; o < len; o += 256)
+		{
+			int nread = len - o;
+			if (nread > 256)
+				nread = 256;
+
+			int nret = file_read(fd, ptr + o, nread);
+			if (nret <= 0)
+				break;
+
+			nbytes += nret;
+		}
+		return nbytes;
 	}
 	else
 	{
