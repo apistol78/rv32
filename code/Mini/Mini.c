63287f92f6e9d42ae3128e488fdaa63aa2709c45
Anders Pistol <anders.pistol@doctorentertainment.com>
2022-09-13 10:06:44 +0200

    Rv32: Removed Mini sample.

--- code/Mini/Mini.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-void main()
-{
-	for (int i = 0; i < 10; ++i)
-		printf("Hello world %d!\n", i);
-}
--- code/Mini/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-../../build/linux/ReleaseStatic/Rv32 -h=../Firmware/Firmware.hex -u=Mini.hex
