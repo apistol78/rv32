a2a501f23afcc587e238c174e70fbc4aaaff5433
Anders Pistol <anders.pistol@doctorentertainment.com>
2023-01-17 17:31:56 +0100

    Rv32: BUS profiler improved.

--- code/Rv32T/ProfilerBUS.cpp
+++ code/Rv32T/ProfilerBUS.cpp
@@ -31,16 +31,23 @@ const wchar_t* c_names[] =
 ProfilerBUS::ProfilerBUS()
 {
 	for (int32_t i = 0; i < sizeof_array(c_names); ++i)
-		m_deviceActive[i] = 0;
+		m_deviceActive[i] = { 0, 0 };
 }
 
 ProfilerBUS::~ProfilerBUS()
 {
 	for (int32_t i = 0; i < sizeof_array(c_names); ++i)
 	{
-		const double pl = (m_deviceActive[i] * 100.0) / m_total;
-		const double pa = (m_deviceActive[i] * 100.0) / m_totalActive;
-		log::info << str(L"%-10S", c_names[i]) << L": " << str(L"%6.2f", pa) << L" %  " << str(L"%6.2f", pl) << L" %." << Endl;
+		const uint32_t rwc = m_deviceActive[i].read + m_deviceActive[i].write;
+		
+		const double pra = (m_deviceActive[i].read * 100.0) / m_totalActive;	// percent of bus active cycles
+		const double prl = (m_deviceActive[i].read * 100.0) / m_total;			// percent of runtime cycles
+		const double pwa = (m_deviceActive[i].write * 100.0) / m_totalActive;	// percent of bus active cycles
+		const double pwl = (m_deviceActive[i].write * 100.0) / m_total;			// percent of runtime cycles
+
+		log::info << str(L"%-10S", c_names[i]) << L": " <<
+			str(L"%6.2f", pra) << L" / " << str(L"%5.2f", pwa) << L" %  " <<
+			str(L"%6.2f", prl) << L" / " << str(L"%5.2f", pwl) << L" %." << Endl;
 	}
 
 	const double p = (m_totalActive * 100.0) / m_total;
@@ -55,7 +62,12 @@ void ProfilerBUS::eval(VSoC* soc, uint64_t time)
 		{
 			const int32_t bit = (sizeof_array(c_names) - 1 - i);
 			if ((soc->o_debug_bus_select & (1 << bit)) != 0)
-				m_deviceActive[i]++;
+			{
+				if (soc->o_debug_bus_rw == 0)
+					m_deviceActive[i].read++;
+				else
+					m_deviceActive[i].write++;
+			}
 		}
 		m_totalActive++;
 	}
--- code/Rv32T/ProfilerBUS.h
+++ code/Rv32T/ProfilerBUS.h
@@ -7,14 +7,20 @@ class ProfilerBUS : public Device
 	T_RTTI_CLASS;
 
 public:
-	explicit ProfilerBUS();
+	ProfilerBUS();
 
     virtual ~ProfilerBUS();
 
 	virtual void eval(VSoC* soc, uint64_t time) override final;
 
 private:
-    uint32_t m_deviceActive[13];
+    struct
+    {
+        uint32_t read;
+        uint32_t write;
+    }
+    m_deviceActive[13];
+
     uint32_t m_total = 0;
     uint32_t m_totalActive = 0;
 };
