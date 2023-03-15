19aa4f1544f27d0061c80d0c6ece335fbc0fd44f
Anders Pistol <anders.pistol@doctorentertainment.com>
2023-03-15 18:07:52 +0100

    Rv32: Added MPL license notice on relevant files.

--- code/Rv32T/CRC.cpp
+++ code/Rv32T/CRC.cpp
@@ -1,3 +1,11 @@
+/*
+ TRAKTOR
+ Copyright (c) 2023 Anders Pistol.
+
+ This Source Code Form is subject to the terms of the Mozilla Public
+ License, v. 2.0. If a copy of the MPL was not distributed with this
+ file, You can obtain one at https://mozilla.org/MPL/2.0/.
+*/
 #include "Rv32T/CRC.h"
 
 /* Table for CRC-7 (polynomial x^7 + x^3 + 1) */
